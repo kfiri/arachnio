8819d34941bd82b4c891802c18097b57073b0a78
kfiri <kfirr99@gmail.com>
2017-08-24 15:02:15 +0300

    added consts and utils files

---
+++ frontend/static/js/consts.js
@@ -0,0 +1,6 @@
+let CONSTS = {
+    INITIAL_SIZE:   10,
+    INITIAL_SPEED:  1.0,  // Blocks per second.
+    INITIAL_POINTS: 0,
+    POINT_SIZE:     1  // The size the player get from a single point.
+};
