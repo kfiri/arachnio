4308a8081adb9f622154df9efc6757c491277711
Guy Sviry <dlobimail@gmail.com>
2017-08-25 05:59:24 +0300

    Added move.

--- backend/game/core.py
+++ backend/game/core.py
@@ -1,6 +1,8 @@
 import json
 from random import randint
 
+from tornado.ioloop import IOLoop
+
 from .player import Player
 from . import message
 from .. import config
@@ -21,11 +23,49 @@ class Game(object):
         self.width = width
         self.height = height
         self.socket_to_player = dict()
+        self.location_to_player = dict()
         self.is_cyclic = is_cyclic
     
+    def move_player(self, player, x_delta, y_delta):
+        if (player.x + x_delta > self.width or player.x + x_delta < 0) and self.is_cyclic:
+            player.x = (player.x + x_delta) % self.width
+        elif (0 < player.x + x_delta < self.width):
+            player.x = player.x + x_delta
+        
+        if (player.y + y_delta > self.height or player.y + y_delta < 0) and self.is_cyclic:
+            player.y = (player.y + y_delta) % self.height
+        elif (0 < player.y + y_delta < self.width):
+            player.y = player.y + y_delta
+
+        player.in_cooldown = True
+        # TODO: Ultimate todo.. see bottom comments
+        IOLoop.current().call_later(player.get_cooldown_duration(), player.disable_cooldown)
+        # Maybe update locations here?
+        players_at_location = self.get_players_at(player.x, player.y)
+        # Solve possible conflicts here..
+        self.update_location_to_players()
+    
+    def get_players_at(self, x, y):
+        return self.location_to_player.get((x, y), [])
+
+    # TODO: replace update function you need to remember calling with automatic solution
+    def update_location_to_players(self):
+        self.location_to_player = dict()
+        for player in self.socket_to_player.values():
+            location = (player.x, player.y)
+            self.location_to_player.setdefault(location, [])
+            self.location_to_player[location].append(player)
+    
     def create_new_player(self, websocket, name):
-        return Player(websocket=websocket, x=randint(0, self.width),
-            y=randint(0, self.height), name=name)
+        while True:
+            player = Player(websocket=websocket, x=randint(0, self.width),
+                y=randint(0, self.height), name=name)
+            if self.get_players_at(player.x, player.y) == []:
+                # No conflicting players
+                break
+        self.socket_to_player[websocket] = player
+        self.update_location_to_players()
+        return player
     
     def send_welcome(self, player_socket):
         player_socket.write_message(json.dumps({
@@ -37,7 +77,7 @@ class Game(object):
             }
         }))
     
-    def send_update(self):
+    def send_info(self):
         game_state = {
             'players': 
                 {player.id: player.prepare_json() for player in self.socket_to_player.values()}
@@ -57,6 +97,6 @@ class Game(object):
     
     def game_iteration(self):
         """
-        A game iteration ran on main loop
+        A game iteration. Executed on main loop
         """
-        self.send_update()
+        self.send_info()
